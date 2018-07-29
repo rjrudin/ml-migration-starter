d831e432e8934ec678c0c435afb95ad2cab994ff
Rob Rudin <rob.rudin@marklogic.com>
2018-07-29 17:54:41 -0400

    cleanup

--- src/test/java/com/marklogic/migration/rdbms/AbstractTest.java
+++ src/test/java/com/marklogic/migration/rdbms/AbstractTest.java
@@ -5,22 +5,54 @@ import com.marklogic.spring.batch.columnmap.ColumnMapSerializer;
 import com.marklogic.spring.batch.columnmap.JacksonColumnMapSerializer;
 import org.example.MigrationConfig;
 import org.junit.Before;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ContextConfiguration(classes = {TestConfig.class, MigrationConfig.class})
 public abstract class AbstractTest extends AbstractSpringTest {
+
+	@Autowired
+	protected JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	protected Environment environment;
 
 	protected JdbcTemplate jdbcTemplate;
 	protected ColumnMapSerializer columnMapSerializer = new JacksonColumnMapSerializer();
 
-	// TODO Replace strings with properties
 	@Before
 	public void setupJdbcTemplate() {
 		DriverManagerDataSource ds = new DriverManagerDataSource(
-			"jdbc:mysql://localhost:3306/sakila?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-			"root", "password");
+			environment.getProperty("jdbc_url"),
+			environment.getProperty("jdbc_username"),
+			environment.getProperty("jdbc_password")
+		);
 		this.jdbcTemplate = new JdbcTemplate(ds);
+	}
+
+	protected Map<String, JobParameter> buildJobParameterMap() {
+		Map<String, JobParameter> params = new HashMap<>();
+		params.put("host", new JobParameter(environment.getProperty("mlHost")));
+		params.put("port", new JobParameter(Long.parseLong(environment.getProperty("mlTestRestPort"))));
+		params.put("username", new JobParameter(environment.getProperty("mlUsername")));
+		params.put("password", new JobParameter(environment.getProperty("mlPassword")));
+		addStringProperty(params, "jdbc_driver");
+		addStringProperty(params, "jdbc_url");
+		addStringProperty(params, "jdbc_username");
+		addStringProperty(params, "jdbc_password");
+		params.put("chunk_size", new JobParameter(Long.parseLong(environment.getProperty("chunk_size"))));
+		return params;
+	}
+
+	protected void addStringProperty(Map<String, JobParameter> params, String name) {
+		params.put(name, new JobParameter(environment.getProperty(name)));
 	}
 }
--- src/test/java/com/marklogic/migration/rdbms/SingleQueryTest.java
+++ src/test/java/com/marklogic/migration/rdbms/SingleQueryTest.java
@@ -1,22 +1,11 @@
 package com.marklogic.migration.rdbms;
 
 import org.junit.Test;
-import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class SingleQueryTest extends AbstractTest {
-
-	@Autowired
-	private JobLauncherTestUtils jobLauncherTestUtils;
-
-	@Autowired
-	private Environment environment;
 
 	@Test
 	public void test() throws Exception {
@@ -25,23 +14,5 @@ public class SingleQueryTest extends AbstractTest {
 		System.out.println(map);
 
 		jobLauncherTestUtils.launchJob(new JobParameters(buildJobParameterMap()));
-	}
-
-	protected Map<String, JobParameter> buildJobParameterMap() {
-		Map<String, JobParameter> params = new HashMap<>();
-		params.put("host", new JobParameter(environment.getProperty("mlHost")));
-		params.put("port", new JobParameter(Long.parseLong(environment.getProperty("mlTestRestPort"))));
-		params.put("username", new JobParameter(environment.getProperty("mlUsername")));
-		params.put("password", new JobParameter(environment.getProperty("mlPassword")));
-		addStringProperty(params, "jdbc_driver");
-		addStringProperty(params, "jdbc_url");
-		addStringProperty(params, "jdbc_username");
-		addStringProperty(params, "jdbc_password");
-		params.put("chunk_size", new JobParameter(Long.parseLong(environment.getProperty("chunk_size"))));
-		return params;
-	}
-
-	protected void addStringProperty(Map<String, JobParameter> params, String name) {
-		params.put(name, new JobParameter(environment.getProperty(name)));
 	}
 }
