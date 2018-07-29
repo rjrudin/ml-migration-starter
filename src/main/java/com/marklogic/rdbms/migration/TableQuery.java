5bb109f5c933594c035cc811b618f3da03124a94
Rob Rudin <rob.rudin@marklogic.com>
2018-07-28 22:13:25 -0400

    More progress

---
+++ src/main/java/com/marklogic/rdbms/migration/TableQuery.java
@@ -0,0 +1,48 @@
+package com.marklogic.rdbms.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableQuery {
+
+	private String query;
+	// TODO Support compound keys
+	private String primaryKeyColumnName;
+	private String elementName;
+
+	// Optional - for child table
+	private String foreignKeyColumnName;
+
+	private List<TableQuery> childQueries = new ArrayList<>();
+
+	public TableQuery(String query, String primaryKeyColumnName, String foreignKeyColumnName, String elementName) {
+		this.query = query;
+		this.primaryKeyColumnName = primaryKeyColumnName;
+		this.foreignKeyColumnName = foreignKeyColumnName;
+		this.elementName = elementName;
+	}
+
+	public void addChildQuery(TableQuery tableQuery) {
+		this.childQueries.add(tableQuery);
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public String getPrimaryKeyColumnName() {
+		return primaryKeyColumnName;
+	}
+
+	public String getElementName() {
+		return elementName;
+	}
+
+	public String getForeignKeyColumnName() {
+		return foreignKeyColumnName;
+	}
+
+	public List<TableQuery> getChildQueries() {
+		return childQueries;
+	}
+}
