0aa0fbeec97a4e7bd7b56291f3e5e66646f0cef1
Scott Stafford <sstafford@marklogic.com>
2018-07-31 21:49:15 -0400

    Set SQL tables to lowercase

--- src/test/java/com/marklogic/migration/rdbms/GrandchildQueryTest.java
+++ src/test/java/com/marklogic/migration/rdbms/GrandchildQueryTest.java
@@ -7,10 +7,10 @@ public class GrandchildQueryTest extends AbstractTest {
 
 	@Test
 	public void test() throws Exception {
-		TableQuery customerQuery = new TableQuery("select * from Customer", "customer_id", null, "customer");
-		TableQuery rentalQuery = new TableQuery("select * from Rental", "rental_id", "customer_id", "rentals");
+		TableQuery customerQuery = new TableQuery("select * from customer", "customer_id", null, "customer");
+		TableQuery rentalQuery = new TableQuery("select * from rental", "rental_id", "customer_id", "rentals");
 		customerQuery.addChildQuery(rentalQuery);
-		TableQuery paymentQuery = new TableQuery("select * from Payment", "payment_id", "rental_id", "payments");
+		TableQuery paymentQuery = new TableQuery("select * from payment", "payment_id", "rental_id", "payments");
 		rentalQuery.addChildQuery(paymentQuery);
 
 		String migrationJson = new ObjectMapper().writeValueAsString(customerQuery);
